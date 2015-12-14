dba819768ab41d097dd0c3efd8b728eb7c0d77a1
Hosup Lee <zzem77@gmail.com>
2015-12-14 14:32:39 -0600

    selectAllMaterialTypes.selectAllMaterialTypes.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http)
+
+app.use('/static', express.static('public'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -9,14 +12,15 @@ app.get('/', function (req, res) {
 var sendLimit = {};
 
 io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
+    socket.on('chat', function (msg) {
         if (typeof sendLimit[socket.id] === 'undefined') {
             sendLimit[socket.id] = 0;
         } else {
             sendLimit[socket.id]++;
         }
 
-        if (sendLimit[socket.id] && sendLimit[socket.id] > 2) {
+        io.emit('chat', msg);
+        if (sendLimit[socket.id] && sendLimit[socket.id] > 9) {
             io.emit('limited');
         } else {
             io.emit('chat message', msg);
